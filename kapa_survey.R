2da9e7b8e59e15e387e95235a309f55eb7be2d07
Andy Roy <andy@improvize.com>
2020-07-20 20:34:47 -0500

    cleanup and checkpoint

--- kapa_survey.R
+++ kapa_survey.R
@@ -6,10 +6,17 @@
 library(data.table)
 library(ggplot2)
 
+source("topicfinder.R")
 
 fn  <- "kapa survey responses 2020-06-29.csv"
 dat <- fread(fn)
 readTopics("kapa phrases.csv")
+
+## Hand Coded classification
+hand <- fread("kapa survey - hand coded.csv")
+hand <- hand[,.(id,code)]
+names(hand)[2]<-"inconsistent"
+
 
 # save rows into alldf (global)
 save_rows <- function(id,tg,df,txt1="") {
@@ -27,7 +34,7 @@ save_rows <- function(id,tg,df,txt1="") {
    # message("save_rows id: ",id," alldf:",nrow(alldf))
 }
 
-# main loop
+## Topic Analysis by counting incidence of key phrases
 alldf <- NULL
 kapa <- NULL
 for (i in 1:nrow(dat)) {
@@ -40,27 +47,32 @@ for (i in 1:nrow(dat)) {
    flags <- flagTopics2(txt1)
    message("id: ",dat$id[i]," txt1: ",length(txt1)," flags: ",nrow(flags))
 
-   flags.sum <- round(colSums(flags)/nrow(flags),2)
+   # flags.sum <- round(colSums(flags)/nrow(flags),2) # normalize by sentence count
+   flags.sum <- colSums(flags)
    lev <- c(rep(0,nrow(flags)),1)
    flags <- rbind(flags,flags.sum)
    flags <- cbind(flags,data.frame(lev=lev))
 
    save_rows(dat$id[i],nrow(flags),flags,c(txt1,""))
-
 }
+# fix column name
+names(alldf)[which(names(alldf)=="wh3n")] <- "when"
 
-## Cluster plot
+
+## PCA and clusters / classification
 kapa_colors <- rainbow(12,alpha=0.5)[2:10]
-# ramp<-colorRamp(c("cyan","orange"))
-# col1<-rgb(ramp(seq(0,1,length=nc)),max=255)
+ramp<-colorRamp(c("lightgray","white"))
+kapa_colors<-rgb(ramp(seq(0,1,length=6)),max=255)
 
-plot_pca_clusters <- function(xy,nc=4,ti="pca",labs=NULL) {
+plot_pca_clusters <- function(xy,nc=4,ti="pca",labs=NULL,factors="") {
    # xy: x,y coord
    # nc: # of clusters
    # ti: title text
    
+   subtitle <- paste(factors,collapse=" ")
    col1 <- kapa_colors[1:nc]
-   plot(xy,col="blue",type="n",pch=19,cex=.5,main=ti,xlab=NA,ylab=NA)
+   plot(xy,col="blue",type="n",pch=19,cex=.5,
+        main=ti,xlab=NA,ylab=NA,cex.sub=0.8,sub=subtitle)
    fit <- kmeans(xy,nc)
    points(xy,col=col1[fit$cluster],pch=19,cex=1.9)
    if (!is.null(labs)) text(xy,labels = labs,cex=0.6)
@@ -71,30 +83,103 @@ plot_pca_clusters <- function(xy,nc=4,ti="pca",labs=NULL) {
 }
 
 ## Analysis
-topics <- search.topics$topics[1][,1]
-cn <- c("id","tag",topics)
-kapa <- alldf[lev==1,..cn]
+# PCA
+topics <- c("LDMTLD","often","when")
+topics <- c("LDMTLD",topics)
+topics <- c("PCREFp","PCVERBp","SMINTEp","DRGERUND","DRINF","PCTEMPp")
+topics <- c("because","when","often")
+topics <- c("when","often","WRDADJ")
+topics <- c("when","often","LDMTLD","CNCAll") # ***
+topics <- c("when","often","because")
 
+# init kapa
+kapa <- alldf[lev==1,-"text"]
 
-# kapa <- kapa[tag>10,]
-kapa <- kapa[coh[,.(id,LDMTLD)],,on="id"]
+# kapa <- kapa[coh,,on="id"] # join coh-metrix
+kapa <- kapa[hand,,on="id"]
 
 # PCA
-topics <- c("LDMTLD","cond","freq")
-topics <- c("LDMTLD",topics)
-
 pca <- prcomp(kapa[,..topics], scale=T)
 xy <- data.frame(x = pca$x[,1], y = pca$x[,2])
 kapa <- cbind(kapa,xy)
 
-fit <- plot_pca_clusters(xy,4,"Kapa",labs=kapa$id)
+fit <- plot_pca_clusters(xy,4,"Kapa",labs=kapa$id,factors=topics)
 kapa$group <- fit$cluster
-
 table(fit$cluster) # how many in each cluster
-favorites <- which(kapa$id %in% c(21,25,29,30))
-cn <- c("id","group",topics)
-kapa[favorites,..cn]
-points(xy[favorites,],col="red",pch=19,cex=1.9)
+
+# favorites <- which(kapa$id %in% c(21,25,29,30))
+# cn <- c("id","group",topics)
+# kapa[favorites,..cn]
+
+# hand coded
+favorites <- which(kapa$code == 1)
+points(xy[favorites,],col="cyan",pch=1,cex=2,lwd=2)
+
+favorites <- which(kapa$code == 2)
+points(xy[favorites,],col="green",pch=1,cex=2,lwd=2)
+
+favorites <- which(kapa$code > 2)
+points(xy[favorites,],col="red",pch=1,cex=2,lwd=2)
+
+
+# Cross Tabulations
+
+cross_tab <- function(v1,v2) {
+
+   nmx <- deparse(substitute(v1))
+   nmx <- strsplit(nmx,"\\$")[[1]][2]; nmx <- gsub("[^A-z]","",nmx)
+   nmy <- deparse(substitute(v2))
+   nmy <- strsplit(nmy,"\\$")[[1]][2]; nmy <- gsub("[^A-z]","",nmy)
+   par(mar=c(6,2,1,1))
+   
+   if (class(v2)!="logical") {
+      med <- summary(v1)[3]
+      brks <- summary(v2)[c(1,2,4,5,6)]
+      if (length(unique(brks))<5) brks[1]<-brks[1]-0.01
+      
+      tb <- table(as.factor(v1>med),
+                  cut(v2,breaks=brks,labels=c("min","low","med","high"),
+                      include.lowest=T,ordered_result = T),
+                  dnn=c(nmx,nmy))
+      xtrue  <- sum(tb[2,3:4]); xfalse <- sum(tb[1,3:4])
+      likely <- round(100*(xtrue-xfalse)/xfalse,1)
+   } 
+   else {
+      tb <- table(as.factor(v1), as.factor(v2), dnn=c(nmx,nmy))
+      pt <- prop.table(tb, margin=1)
+      likely <- round(100*(pt[2,2]/pt[1,2]-1),1)
+   }
+
+   plot(tb,col=topo.colors(5)[2:5],main=paste0(nmx," => ",nmy),
+        sub=paste0("\nmore likely: ",likely,"%"),cex.axis=0.6)
+   return(tb)
+}
+
+cross_tab(kapa$when>1.5,kapa$inconsistent>0)
+cross_tab(kapa$often>1,kapa$inconsistent>0)
+cross_tab(kapa$because,kapa$inconsistent)
+
+
+### EXTRA / LEFTOVERS
+
+p1 <- xy[c(4,15),] # two points the form a line
+m1 <- (p1$y[2]-p1$y[1])/(p1$x[2]-p1$x[1])
+abline(a=0,b=m1) # parallel line
+
+line_side <- function(xy,p1) {
+   # xy: point to be tested
+   # p1: two points forming line
+   # output: 1 or 2
+   # formula: d=(x−x1)(y2−y1)−(y−y1)(x2−x1)
+
+   d <- (xy$x-p1$x[1])*(p1$y[2]-p1$y[1]) - (xy$y-p1$y[1])*(p1$x[2]-p1$x[1])
+   if (d<0) return(1)
+   return(2)
+}
+
+x1 <- xy[which(kapa$id==11),]
+line_side(x1,p1)
+
 
 # look at only longer responses
 kapa <- kapa[tag>10,]
@@ -105,13 +190,9 @@ kapa <- kapa[group==3,]
 dat.show <- dat[kapa,,on="id"]
 write.csv(dat.show,"kapa survey subset-15JUL.csv",row.names = F)
 
-# cnk <- c("tag","trait","cond","freq","worse","group","because","id")
-
-
+# cnk <- c("tag","myself","when","often","worse","group","because","id")
 
 # barplot(flags.sum,main = paste("Respondent",dat$id[i],collapse = " "))
-
-readTopics("kapa phrases.csv")
 
 # flags <- flagTopics()
 # 
@@ -122,7 +203,7 @@ readTopics("kapa phrases.csv")
 # create an example to work with
 fsum <- flags.sum[rep(1,10),]
 fsum <- cbind(id=1:10,fsum)
-fsum$trait<-round(runif(10,1,8))
+fsum$myself<-round(runif(10,1,8))
 fsum$time<-round(runif(10,1,8))
 fsum$freq<-round(runif(10,1,8))
 fsum$extreme<-round(runif(10,1,8))
@@ -244,16 +325,72 @@ htmlFile <- file.path(tempDir, "test.html")
 writeLines(as.character(doc), htmlFile)
 rstudioapi::viewer(htmlFile)
 
+##
 ## Coh-Metrix data import
+##
 coh <- fread("cohmetrix-POSNEGcombined.csv")
 coh.desc <- as.character(coh[1,])
 names(coh.desc) <- names(coh)
 write.csv(coh.desc,"cohmetrix-fields.csv")
 
-coh <- coh[-1,]
-names(coh)[1] <- "id"
+coh <- coh[-1,] # eliminate descriptions
+names(coh)[1] <- "id" # rename "Numbers" to "id"
 write.csv(coh,"coh-metrix kapa.csv",row.names = F)
+coh <- fread("coh-metrix kapa.csv") # reading back in changes chr columns to numeric
 
-coh <- fread("coh-metrix kapa.csv")
+cat(grep("cnc",names(coh.desc),ignore=T,value=T))
 
+# lexical diversity, any connective measures (start with CNC), and 
+# referential cohesion (PCREFp)
+# 
+# also the following categories might be useful:
+# 
+# verb cohesion (PCVERBp)- which might also be expected logically
+# Intentional verb incidence (SMINTEp)
+# Gerund density (DRGERUND)
+# Infinitive density (DRINF)
+# 
+# MAYBE temporality (PCTEMPp)... to pick up some of the "hedging
 
+## from DAN
+# -- qualities that contradict (hard working / lazy being the most common)
+# -- qualifier statements – or one could simply say “contextualizing statements,” 
+# i.e., some people write about other people, places, events, challenges 
+# when asked to describe themselves, 
+# whereas others describe isolated in-the-head attributes
+# -- connector statements:  “x makes me y” (e.g., my depression makes me shy); 
+# “X causes my y”. 
+# 
+# Running beyond our data per se, some people seem automatically to draw upon a kind of story-schematic sense of person attributes, whereas others self-describe in a set of seemingly independent attribute terms.
+
+### AWS Comprehend
+
+library(aws.comprehend)
+getwd()
+setwd("/Users/anindaroy/Google Drive/RCode")
+x<-read.csv("aws_credentials.csv",stringsAsFactors = F)
+Sys.setenv(AWS_ACCESS_KEY_ID=x$Access.key.ID)
+Sys.setenv(AWS_SECRET_ACCESS_KEY=x$Secret.access.key)
+
+aws1 <- detect_entities(text=alldf$text[1],region='us-east-2')
+aws1 <- detect_phrases(text=txt,region='us-east-2')
+aws2 <- detect_syntax(txt)
+aws2 <- as.data.table(aws2)
+aws2$Text<-tolower(aws2$Text)
+
+aws2[PartOfSpeech.Tag=="ADP",]
+aws2[Text=="when",]
+names(aws2)
+names(aws2)[5]<-"pos"
+w1 <- which(aws2$pos=="CONJ")
+sapply(w1, function(x) paste(aws2[(x-2):(x+2),Text],collapse = " ") )
+
+aws2[pos=="PUNCT",.N]
+aws2<-aws2[pos!="PUNCT",]
+aws2.pairs <- paste(c("",aws2$Text),c(aws2$Text,""))
+
+grep("i am",aws2.pairs)
+sapply(grep("i am",aws2.pairs), function(x) paste(aws2[(x-1):(x+4),Text],collapse = " ") )
+# [1] "i am a hard and diligent"        "i am honest and will tell"      
+# [3] "i am a good listener and"        "i am strong and have proven"    
+# [5] "i am independent but am willing" "i am sociable and enjoy talking"
